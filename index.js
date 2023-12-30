633166694a852322c50e97aa9fb3b5ca567dd0e2
Granton <waribekihiko651@gmail.com>
2023-12-30 18:58:37 +0300

    formating

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 var request = require('request');
 
 const app = express();
-
 app.use(express.json())
 app.set('view engine', 'ejs')
 app.use(express.urlencoded())
